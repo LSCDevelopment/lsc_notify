2f629edffd42ff95a02c9e08da0acd943b8b0412
RK <rkfrmda3@gmail.com>
2025-05-06 14:08:59 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'LSC Development - Rkfrmda3'
+description 'A standalone notify, inspired by gtaworld.'
+
+ui_page 'web/index.html'
+
+files {
+    'web/index.html',
+    'web/assets/**',
+}
+
+client_scripts {
+   'client.lua'
+}
+
